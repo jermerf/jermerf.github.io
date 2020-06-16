b934a7e6d8ae101acb15c15f66f4d6d0b510c72b
Jermaine <jermerf@gmail.com>
2020-06-16 07:48:54 -0300

    Default Vue PWA

---
+++ precache-manifest.25f50a0dee5969fd7aff6efd303b4fc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4680f376cf21bc683b7f",
+    "url": "/css/app.b1b67026.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "673585ec73fca7ed3c399b343b9c55a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4680f376cf21bc683b7f",
+    "url": "/js/app.0b76e2a4.js"
+  },
+  {
+    "revision": "ac79504e6ac0b6cf6b19",
+    "url": "/js/chunk-vendors.a092b9a3.js"
+  },
+  {
+    "revision": "41fad960d54410b30ea1a005ec235727",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
